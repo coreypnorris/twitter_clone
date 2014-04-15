a4a02d097cb4e60977c4bab18f926b4f8cc9f6d1
Corey Norris & Elikem Adadevoh <student@epicodus.com>
2014-04-15 15:03:04 -0700

    adds new mailer functionality

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ActionMailer::Base
+  default from: "no_reply@tweety.com"
+
+  def welcome_message(user)
+    @user = user
+    mail to: @user.email, subject: 'Welcome to Tweety',
+  end
+end
